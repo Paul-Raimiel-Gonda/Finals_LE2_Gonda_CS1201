bb02c0499173408a887824549de7b496e2cd1a9e
Paul-Raimiel-Gonda <157487413+Paul-Raimiel-Gonda@users.noreply.github.com>
2024-05-16 23:27:10 +0800

    change font color

--- FINALS_LE2_GONDA/utils/dice_game.py
+++ FINALS_LE2_GONDA/utils/dice_game.py
@@ -18,12 +18,14 @@ class DiceGame:
         total_score = 0
         total_rounds_won = 0
         current_stage = 1 
+        
+        
 
         while True:  
             stage_score = 0
             stage_rounds_won = 0
             os.system('cls')
-            print(f"\nStage {current_stage}\n")
+            print(f"\nStage {current_stage} ✧\n")
 
             for _ in range(self.total_rounds):  #generate a random integer 1-6 for both the user and cpu
                 user_number = random.randint(1, 6)
@@ -56,8 +58,8 @@ class DiceGame:
 
             if total_rounds_won == self.total_rounds:  
                 total_score += 3  #Add additional 3 points if you've won all 3 rounds in a stage
-                print("Congratulations! You won all rounds in this stage and gained 3 additional points!")
-                print(f"Total Score: {total_score}")
+                print("Congratulations! You won all rounds in this stage and gained 3 ✧ additional points!")
+                print(f"Total Score: {total_score} ✧")
 
             choice = input("Do you want to proceed to the next stage? (1 for yes, 0 for no): ") #ask the user if they want to proceed to the next stage
             while choice not in ('1', '0'):
@@ -68,8 +70,8 @@ class DiceGame:
 
             current_stage += 1 
 
-        print(f"\nTotal Points Earned: {total_score}")
-        print(f"Number of Rounds Won: {total_rounds_won}")
+        print(f"\nTotal Points Earned: {total_score} ✧")
+        print(f"Number of Rounds Won: {total_rounds_won} ✧")
 
         score_manager = Score()  
         score_manager.save_scores(user.username, total_score, total_rounds_won, datetime.now().strftime("%Y-%m-%d %H:%M:%S")) #track the data
@@ -82,7 +84,7 @@ class DiceGame:
         if scores:
             scores.sort(key=lambda x: int(x[1]), reverse=True)  #SORT THE SCORES INTO HIGHEST TO LOWEST
             os.system('cls')
-            print("\nTop 10 Scores:")
+            print("\n✧ Top 10 Scores:")
             for i, (username, score, rounds_won, date) in enumerate(scores[:10], 1):             
                 print(f"{i}. {username}: {score}, Rounds Won: {rounds_won}, Date: {date}")
         else:
@@ -113,17 +115,25 @@ class DiceGame:
 #THE MENU FOR DICE GAME
     def game_menu(self, user):
         os.system('cls')
+        CEND = '\33[0m'
+        CMAGENTA = '\33[35m'
+        CBOLD = '\33[1m'
+        
         while True:
-            print("""\n
-       _____                               _____                                 
-    __|__   |__  ____  ______  ______   __|___  |__  ____    ____    __  ______  
-   |     \     ||    ||   ___||   ___| |   ___|    ||    \  |    \  /  ||   ___| 
-   |      \    ||    ||   |__ |   ___| |   |  |    ||     \ |     \/   ||   ___| 
-   |______/  __||____||______||______| |______|  __||__|\__\|__/\__/|__||______| 
-      |_____|                             |_____|                                
-                                                                                 \n""")
+            os.system('cls')
+            print(CMAGENTA + CBOLD + """
+    ================================================================================
+        _____                               _____                                 
+     __|__   |__  ____  ______  ______   __|___  |__  ____    ____    __  ______  
+    |     \     ||    ||   ___||   ___| |   ___|    ||    \  |    \  /  ||   ___| 
+    |      \    ||    ||   |__ |   ___| |   |  |    ||     \ |     \/   ||   ___| 
+    |______/  __||____||______||______| |______|  __||__|\__\|__/\__/|__||______| 
+        |_____|                             |_____|                                
+                                                                                    
+    ================================================================================
+    """ + CEND)
             print("\t\t\t1. Play Game")
-            print("\t\t\t2. Show Top Scores")
+            print("\t\t\t2. Show Top Scores ✧")
             print("\t\t\t3. Show Your Scores")
             print("\t\t\t4. Logout")
 
